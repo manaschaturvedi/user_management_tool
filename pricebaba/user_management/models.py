4ea72e338a9711e58d341ed93e0696caaea52063
Manas Chaturvedi <manas.oid@gmail.com>
2017-10-15 23:21:03 +0530

    changes in dob field schema

--- pricebaba/user_management/models.py
+++ pricebaba/user_management/models.py
@@ -8,7 +8,7 @@ class Pricebaba_Users(models.Model):
 	email = models.EmailField()
 	mobile = models.CharField(max_length=15)
 	age = models.IntegerField()
-	dob = models.IntegerField()
+	dob = models.CharField(max_length=100)
 	location = models.CharField(max_length=255)
 	updated_on = models.DateTimeField(auto_now=True, auto_now_add=False)
 	created_on = models.DateTimeField(auto_now=False,auto_now_add=True)
